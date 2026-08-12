122a5dbd235df335862b4e079911ef98131fe077
skarthik112 <147787819+skarthik112@users.noreply.github.com>
2026-08-12 11:14:21 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/sort-colors/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int i=0,j=0,k=nums.length-1;
+        while(j<=k){
+            if(nums[j]==1){
+                j++;
+            }else if(nums[j]==2){
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+
+                k--;
+            }else{
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+            }
+        }
+    }
+}
